c093943257fabaee2c5a8548ed3d59f4a8a4d555
John Lai <jlai@carpages.ca>
2021-04-30 15:12:03 -0400

    #1 adding functions to remove existing array selected

--- script.js
+++ script.js
@@ -7,84 +7,106 @@
 // It will take a user input for them to answer.
 // after timer it will show results.
 
-const questionGoesHere = $(".questionGoesHere");
-const iImageId = document.querySelector(".i-image");
+//use init
+// toDoApp.init();
+const aslQuiz = {};
 
-const submitBtn = $('input[type="submit"]');
-const userAnswer = $('input[id="userAnswer"]');
-const correctOrNot = $(".correctOrNot");
+aslQuiz.init = function () {
+  aslQuiz.originalArray = ["a", "c", "i", "r", "y"];
+  aslQuiz.questionGoesHere = $(".questionGoesHere");
+  aslQuiz.formSubmit = $(".submitForm");
+  aslQuiz.userAnswer = $('input[id="userAnswer"]');
+  aslQuiz.correctOrNot = $(".correctOrNot");
+  aslQuiz.correctPercentage = $(".correctPercentage");
+  aslQuiz.iImageId = document.querySelector(".i-image");
+  aslQuiz.points = 0;
 
-const questions = ["a", "c", "i", "r", "y"];
-let points = 0;
-const correctPercentage = $(".correctPercentage");
+  //Get a random question from the array
+  const randomizeQuestion = () => {
+    //grab one of the item from the array randomly.
+    if (aslQuiz.originalArray.length) {
+      const chosenAlphabet =
+        aslQuiz.originalArray[
+          Math.floor(Math.random() * aslQuiz.originalArray.length)
+        ];
 
-//remove the item grabed
+      const indexOfChosenAlphaBet = aslQuiz.originalArray.indexOf(
+        chosenAlphabet
+      );
+      return { chosenAlphabet, indexOfChosenAlphaBet };
+    } else {
+      alert(`Congrats You're done!`);
+    }
+  };
 
-//Get a random question from the array
-const randomizeQuestion = () => {
-  //grab one of the item from the array randomly.
-  return questions[Math.floor(Math.random() * questions.length)];
+  //function to update question
+  const updateQuestion = (selectedQuestion) => {
+    console.log(selectedQuestion);
+    aslQuiz.questionGoesHere.html(selectedQuestion);
+    //updating image path with random number
+    aslQuiz.iImageId.src = `img/${selectedQuestion}.svg`;
+  };
+
+  //Update correct number here
+  const updateCorrectNumber = (points) => {
+    aslQuiz.correctPercentage.text(points);
+  };
+
+  // if answer match add to total coorect number
+  const checkIfCorrect = (userAnswerValue, selectedQuestion) => {
+    console.log(userAnswerValue, selectedQuestion);
+    if (userAnswerValue.toLowerCase() === selectedQuestion) {
+      aslQuiz.points += 1;
+      console.log(aslQuiz.points);
+      aslQuiz.correctOrNot.html("yes baby");
+      updateCorrectNumber(aslQuiz.points);
+    } else {
+      aslQuiz.correctOrNot.html("Wrong Son");
+      console.log("wrong");
+    }
+  };
+
+  //on submit trigger update question
+  aslQuiz.formSubmit.on("submit", function (e) {
+    e.preventDefault();
+    const userAnswerValue = aslQuiz.userAnswer.val();
+
+    if (!userAnswerValue) {
+      alert("input something fam");
+    } else {
+      // get input answer
+      console.log(userAnswerValue);
+      checkIfCorrect(userAnswerValue, aslQuiz.selectedQuestion.chosenAlphabet);
+      //getting next question here
+      aslQuiz.selectedQuestion = randomizeQuestion();
+      updateQuestion(aslQuiz.selectedQuestion.chosenAlphabet);
+
+      removeQuestionFromArray(aslQuiz.selectedQuestion.indexOfChosenAlphaBet);
+
+      // clearValues
+      aslQuiz.userAnswer.val("");
+    }
+  });
+
+  //Remove array clicked on
+  const removeQuestionFromArray = (indexOfSelectedQuuestion) => {
+    //if array is not empty execute
+    if (aslQuiz.originalArray.length) {
+      aslQuiz.originalArray.splice(indexOfSelectedQuuestion, 1);
+      console.log(aslQuiz.originalArray);
+    }
+  };
+
+  //on load we want to trigger a random question and update the question, and userAnswerInput will check to see if it is a correct value;
+  aslQuiz.selectedQuestion = randomizeQuestion();
+  randomizeQuestion();
+  updateQuestion(
+    aslQuiz.selectedQuestion.chosenAlphabet,
+    aslQuiz.selectedQuestion.indexOfChosenAlphaBet
+  );
+  removeQuestionFromArray(aslQuiz.selectedQuestion.indexOfChosenAlphaBet);
 };
 
-//function to update question
-const updateQuestion = (selectedQuestion) => {
-  // let randomQuestion = randomizeQuestion;
-  questionGoesHere.html(selectedQuestion);
-  //updating image path with random number
-  iImageId.src = `img/${selectedQuestion}.svg`;
-};
-
-//Update correct number here
-const updateCorrectNumber = (points) => {
-  correctPercentage.text(points);
-};
-
-// if answer match add to total coorect number
-const checkIfCorrect = (userAnswerValue, selectedQuestion) => {
-  console.log(userAnswerValue, selectedQuestion);
-  if (userAnswerValue === selectedQuestion) {
-    points += 1;
-    console.log(points);
-    correctOrNot.html("yes baby");
-    updateCorrectNumber(points);
-  } else {
-    correctOrNot.html("Wrong Son");
-    console.log("wrong");
-  }
-};
-
-//on submit trigger update question
-submitBtn.on("click", function (e) {
-  e.preventDefault();
-  const userAnswerValue = userAnswer.val();
-
-  if (!userAnswerValue) {
-    alert("input something man");
-  } else {
-    // get input answer
-    console.log(userAnswerValue);
-
-    checkIfCorrect(userAnswerValue, selectedQuestion);
-
-    //getting next question here
-    selectedQuestion = randomizeQuestion();
-    updateQuestion(selectedQuestion);
-
-    // clearValues
-    userAnswer.val("");
-  }
+$(function () {
+  aslQuiz.init();
 });
-
-// remove asked qusetion from array
-// const removeQuestionFromArray = (questions) => {
-//   const index = questions.indexOf(selectedQuestion);
-//   questions.splice(index, 1);
-//   console.log(questions);
-// };
-// removeQuestionFromArray();
-
-//on load we want to trigger a random question and update the question, and userAnswerInput will check to see if it is a correct value;
-let selectedQuestion = randomizeQuestion();
-randomizeQuestion();
-updateQuestion(selectedQuestion);
-console.log(selectedQuestion);
